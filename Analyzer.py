638ee6fac272a2e640ae7820ba340c7a401a6d8a
Leo-Zubiri <leo.zubiri@outlook.com>
2022-09-22 23:59:27 -0500

    first commit

---
+++ Analyzer.py
@@ -0,0 +1,91 @@
+import re # Regular Expressions
+class AnalizadorSintactico:
+    VARIABLES = set()
+    TERMINALES = set()
+    INICIAL = ""
+    RULES = []
+    RESULT = []
+    PILA = []
+    ENTRADA = []
+    ACCION = []
+    CADENA = ""
+    ITERACIONES = 0
+
+    def MetodoDesplazamientoReduccion(self,_rules,_variables,_terminales,_inicial,_cadena):
+        self.RULES = _rules
+        self.VARIABLES = _variables
+        self.TERMINALES = _terminales
+        self.INICIAL = _inicial
+        self.CADENA = _cadena
+        
+        self.VARIABLES = list(self.VARIABLES)
+        self.VARIABLES.sort(reverse=True)
+
+        self.TERMINALES = list(self.TERMINALES)
+        self.TERMINALES.sort(reverse=True)
+
+        print(self.CADENA)
+
+        self.CalcularMDR()
+
+        
+    def CalcularMDR(self):
+
+        self.PILA = "$"
+        self.ENTRADA = self.CADENA
+        self.ACCION = "Desplazamiento"
+
+        print("------------------------------------")
+        print("PILA","  |  ENTRADA","  | ACCION")
+
+        while self.ENTRADA != "" or cambioDetectado == True:
+            print(self.PILA,"  |  ",self.ENTRADA,"$  |  ",self.ACCION)
+            cambioDetectado = False
+            self.ITERACIONES += 1
+
+            for regla in self.RULES:
+                #Si se puede aplicar una regla en la pila
+                if regla[1] in self.PILA:
+                    # Reemplazar la regla por la variable correspondiente
+                    self.PILA = self.PILA.replace(regla[1],regla[0])
+                    cambioDetectado = True
+                    self.ACCION = "REGLA "+regla[1]
+
+
+            # Pasar siguiente elemento a la izquierda
+
+            # Si es una variable
+            if cambioDetectado == False:
+                for var in self.VARIABLES:
+                    if re.search("^{}".format(var), self.ENTRADA):
+                        self.PILA = self.PILA + self.ENTRADA[:len(var)] 
+                        self.ENTRADA = self.ENTRADA[len(var)::]
+                        cambioDetectado = True
+                        self.ACCION = "Desplazamiento"
+
+            if cambioDetectado == False:
+                # Pasar el terminal
+                for T in self.TERMINALES:
+                    if T in ["(",")","[","]","+","*","-","/"]:
+
+                        if cambioDetectado == False:
+                            # Caracter especial para el REGEX
+                            if re.search("^\{}".format(T), self.ENTRADA):
+                                self.PILA = self.PILA + self.ENTRADA[:1] 
+                                self.ENTRADA = self.ENTRADA[1::]
+                                cambioDetectado = True
+                                self.ACCION = "Desplazamiento"
+                    else:
+                        if cambioDetectado == False:
+                            if re.search("^{}".format(T), self.ENTRADA):
+                                self.PILA = self.PILA + self.ENTRADA[:len(T)] 
+                                self.ENTRADA = self.ENTRADA[len(T):]
+                                cambioDetectado = True
+                                self.ACCION = "Desplazamiento"
+
+
+        if self.PILA[1:] == self.INICIAL:
+            
+            print("CADENA ACEPTADA")
+        else: 
+            print("CADENA NO ACEPTADA")
---
+++ Main.py
@@ -0,0 +1,87 @@
+"""
+Instrucción para convertir la ventana .ui en .py
+pyuic5 Window.ui -o Window.py
+"""
+import Analyzer
+
+from statistics import linear_regression
+import sys
+from PyQt5 import uic, QtWidgets
+
+qtCreatorFile = "Window.ui"  # Nombre del archivo aquí.
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        # Área de los Signals
+        self.btnGenerar.clicked.connect(self.Generar)
+
+        # Área de variables
+        self.Rules = []
+        self.Variables = set()
+        self.Terminales = set()
+        self.Inicial = ""
+        self.Output = ""
+        self.Input = []
+
+        self.analyze = Analyzer.AnalizadorSintactico()
+        
+    # Área de los Slots
+
+    def Generar(self):
+        print("Generando")
+
+        self.Rules = self.txtRules.toPlainText().replace(" ","").split('\n')
+        self.Variables = self.txtVariables.toPlainText().replace(" ","").split('\n')
+        self.Terminales = self.txtTerminales.toPlainText().replace(" ","").split('\n')
+        self.Inicial = self.txtInicial.toPlainText().replace(" ","")
+        self.Input = self.txtInput.toPlainText().replace(" ","")
+
+        mapaRulesValores = self.mapearInput(self.Rules) 
+        conjVariables = self.conjuntoElementos(self.Variables)  
+        conjTerminales = self.conjuntoElementos(self.Terminales)
+        nodoInicial = self.Inicial
+        cadena = self.Input
+        print("Cadena",cadena)
+
+        res = self.analyze.MetodoDesplazamientoReduccion(mapaRulesValores,conjVariables,conjTerminales,nodoInicial,cadena)
+
+        # #[{aB:'a'},{}]
+        # resFormato = ""
+
+        # for cadenaCalc in res:
+        #     for cadena in cadenaCalc:
+        #         resFormato += "Sig({}) = {} \n".format(cadena,cadenaCalc[cadena])
+
+        # print(res)
+        # print(resFormato)
+
+        # self.txtOutput.setPlainText(resFormato)
+
+
+    def conjuntoElementos(self,lista = []):
+        conjunto = set()
+        for linea in lista:
+            linea = linea.split(',')
+            conjunto.update(set(linea))
+        return conjunto
+        
+    def mapearInput(self,lista=[]):
+        reglas = []
+        for linea in lista:
+            if "->" in linea:
+                linea = linea.split('->')
+                var = linea[0]
+                regla = linea[1]
+                reglas.append([var,regla])
+        return reglas
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
